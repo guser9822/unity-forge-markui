486caa33e296406a4ed21c8550e8a5b6c3b16183
guser9822 <riccardo.daniello13@gmail.com>
2018-01-14 12:38:37 +0100

    Files which contains error messages committed

---
+++ Assets/Script/UIInfoLayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TC.GPConquest.MarkLight4GPConquest
+{
+    public static class UIInfoLayer
+    {
+
+        #region Error messages
+        public static readonly string ServerOptsNullMessage = "Server options can't be null.";
+        //[MapTo("ServerOptsNullMessage")]
+        //public _string OptsNull;
+        #endregion
+    }
+
+}
