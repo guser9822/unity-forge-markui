beef7661a63a17a027276f902fafd8d23c1bf59c
guser9822 <riccardo.daniello13@gmail.com>
2018-01-26 07:32:58 +0100

    Started adding client controller - Need to substitute ServerOptions [WIP]

--- Assets/GPConquest/Script/Client/GameController.cs
+++ Assets/GPConquest/Script/Client/GameController.cs
@@ -1,17 +1,35 @@
-﻿//using BeardedManStudios.Forge.Networking;
-using BeardedManStudios.Forge.Networking.Unity;
-//using BeardedManStudios.SimpleJSON;
-using System.Collections;
-using System.Collections.Generic;
+﻿using BeardedManStudios.Forge.Networking.Unity;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using TC.GPConquest.MarkLight4GPConquest;
 
-public class GameController : MonoBehaviour {
+namespace TC.GPConquest {
 
-    // Use this for initialization
-    void Start () {
-        NetworkManager.Instance.InstantiateDwarf(0,new Vector3(10,10,10));
-        //NetworkManager.Instance.InstantiateDwarfNetworkObject();
+    public class GameController : MonoBehaviour
+    {
+
+        private ClientNetworkController ClientNetworkController;
+        private ServerOptions ServerOption;
+               
+        void Awake()
+        {
+            ClientNetworkController = GetComponent<ClientNetworkController>();
+            ServerOption = gameObject.AddComponent<ServerOptions>();
+
+            ServerOption.SetServerOptions();
+
+            ServerOption.XProtocol.Value = UIInfoLayer.UDPString;
+
+            ClientNetworkController.StartCustomNetworkController(ServerOption);
+
+        }
+
+        // Use this for initialization
+        void Start()
+        { 
+            NetworkManager.Instance.InstantiateDwarf(0, new Vector3(10, 10, 10));
+            //NetworkManager.Instance.InstantiateDwarfNetworkObject();
+        }
+
     }
 
 }
